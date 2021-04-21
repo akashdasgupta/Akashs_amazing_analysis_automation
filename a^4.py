d3da4a7c0290cde1f942d0f4e2c8b9a98beb36b3
Akash Dasgupta <akashdasgupta@physics.ox.ac.uk>
2021-04-21 18:24:40 +0100

    now displays data with/without stabalised outputs via interpolation

--- a^4.py
+++ a^4.py
@@ -9,6 +9,9 @@ lives inside it. You can run this using the command
 
 import os
 import csv
+from scipy.interpolate import interp1d
+import numpy as np 
+
 
 try:
     import originpro as op
@@ -117,7 +120,7 @@ def find_ext(ext, path):
 
 
 def find_starts_with(starts_with, path):
-    """Returns list of files Starts with with some phrase"""
+    """Returns list of files ending with some extention"""
     file_paths = []
     # Loops over files, adds them to blank:
     for root, dirs, files in os.walk(path):
@@ -147,23 +150,6 @@ Thanks for running my software. For help: akash.dasgupta@physics.ox.ac.uk
 
 
 def create_db(path):
-    """
-    For a given path with wavelabs data, will open all the data files (.tsv),
-    sort them by device, and push all data for the same device in a PixelData
-    instance.
-    
-    Parameters
-    ----------
-    path : str
-        Path to where the data lives.
-
-    Returns
-    -------
-    db : dict
-        Dictionary with all the data. Key = Device id string, value= PixelData
-        instance
-
-    """
     db = {}  # This dude will hold all the PixelData instances
 
     csv_file_path = find_ext('.csv', path)[0]  # Assuming only 1 csv
@@ -176,14 +162,18 @@ def create_db(path):
                 skip = False
                 continue
             # The key is actually also how the filename starts:
-            db[row[1] + '_' + row[2] + '_' + "device" + row[7]] = PixelData(row[1],
-                                                                            row[2],
-                                                                            row[3],
-                                                                            row[4],
-                                                                            row[5],
-                                                                            row[6],
-                                                                            row[7],
-                                                                            row[8], )  # Populates ID
+            if row[2] == '':
+                key_str = row[1] + '_' + "device" + row[7]
+            else:
+                key_str = row[1] + '_' + row[2] + '_' + "device" + row[7]
+            db[key_str] = PixelData(row[1],
+                                    row[2],
+                                    row[3],
+                                    row[4],
+                                    row[5],
+                                    row[6],
+                                    row[7],
+                                    row[8], )  # Populates ID
     # Loops over each pixel, sweep through and mines the data:
     for key in db.keys():
         files_in_class = find_starts_with(key, path)
@@ -229,18 +219,6 @@ def create_db(path):
 
 
 def origin_create_plots(db):
-    """
-    For each device in a database, plots an J/V curve in a new folder.
-    Calculates Open circuit voltage, short circuit current, voltage and current
-    at max power point and  fill factor, puts it in a summary folder, and links
-    the j/v curves to each row in the summary matrix
-
-    Parameters
-    ----------
-    db : dict
-        Database of PixelData instances
-
-    """
     # Creates the folders needed:
     op.lt_exec('pe_cd /; pe_mkdir "SUMMARY"; pe_cd /;pe_mkdir "FULL_IV_CURVES";')
 
@@ -250,6 +228,15 @@ def origin_create_plots(db):
     ff_list = []  # Fill factor
     vm_list = []  # Voltage at max power
     im_list = []  # Current at max power
+    pce_list = []
+    # stabalised values:
+    voc_st_list = []  # Open circuit voltage
+    isc_st_list = []  # Short circuit current
+    ff_st_list = []  # Fill factor
+    vm_st_list = []  # Voltage at max power
+    im_st_list = []  # Current at max power
+    pce_st_list = []
+
     ids = []  # The ID dict (for info cols)
     graph_strs = []  # Holds hyperlinks to plotted graphs
 
@@ -264,51 +251,110 @@ def origin_create_plots(db):
         # super hacky way to get more cols but like leave me alone
         for i in range(10):
             op.lt_exec('wks.addCol()')
+            
+        ######################################################################  
+        # Extract data from PixelData class for convenience:
+        area = float(db[key].get_id()["area"])
+        dark_area = float(db[key].get_id()["dark_area"])
+
+        dx1 = db[key].get_dark_iv()[0][0]
+        try:
+            dy1 = [i *(1e3 / dark_area) for i in db[key].get_dark_iv()[0][1]]
+        except:
+            dy1 = None
+        dx2 = db[key].get_dark_iv()[1][0]
+        try:
+            dy2 = [i *(1e3 / dark_area) for i in db[key].get_dark_iv()[1][1]]
+        except:
+            dy2 = None
+
+        lx1 = db[key].get_light_iv()[0][0]
+
+        try:
+            ly1 = [i *(1e3 / area) for i in db[key].get_light_iv()[0][1]]
+        except:
+            raise ValueError("You need to have at least 1 light JV!!!")
+        lx2 = db[key].get_light_iv()[1][0]
+        try:
+            ly2 = [i *(1e3 / area) for i in db[key].get_light_iv()[1][1]]
+        except:
+            ly2 = None
 
         ######################################################################
         # Calculate the parameters:
-        area = float(db[key].get_id()["area"])
-        dark_area = float(db[key].get_id()["dark_area"])
-        voc = float(db[key].get_vt()[0][-1])  # Open circuit voltage, from stability file
-        isc = float(db[key].get_it()[1][-1])  # Short circuit current, from stability file
 
-        # Fill factor: Finds max power, and vm, im in the process, takes ratio of that to voc*isc:
-        mpp_i = db[key].get_mppt()[1]
-        mpp_v = db[key].get_mppt()[0]
-        mpp = min([mpp_i[i] * mpp_v[i] for i in range(len(mpp_v))])
         
-        im = 0
-        vm = 0
-        mpp = 0
-        for i in range(5):
-            im += mpp_i[-i-1]
-            vm += mpp_v[-i-1]
-            mpp += mpp_i[-i-1] * mpp_v[-1]
-        im /= 5
-        vm /= 5
-        mpp /= 5
-        
-        ff = mpp / (voc * isc)
+       # Stability file data: 
 
-        voc_list.append(voc)
-        isc_list.append(isc)
-        ff_list.append(ff)
-        vm_list.append(vm)
-        im_list.append(im)
+        try:
+            voc_st = float(db[key].get_vt()[0][-1])  # Open circuit voltage, from stability file
+        except:
+            voc_st = None
+        try:
+            isc_st = float(db[key].get_it()[1][-1])* (1e3/area)  # Short circuit current, from stability file
+        except:
+            isc_st = None
+        try:
+            im_st = np.mean(db[key].get_mppt()[1][-5:])* (1e3/area)
+            vm_st = np.mean(db[key].get_mppt()[0][-5:])
+            mpp_st = vm_st * im_st 
+        except:
+            im_st, vm_st, mpp_st = (None, None, None)
+        try:
+            ff_st = mpp_st/ (voc_st * isc_st * 10)
+        except:
+            ff_st = None
+  
+
+        j_from_v = interp1d(lx1, ly1)
+        v_from_j = interp1d(ly1, lx1)
+        voc = v_from_j(0)
+        isc = j_from_v(0)
+
+        temp_v = np.arange(min(lx1), max(lx1), (max(lx1)-min(lx1)/10000))
+        mpp_index = np.argmin(temp_v*j_from_v(temp_v))
+        vm = temp_v[mpp_index]
+        im = j_from_v(temp_v[mpp_index])
+
+        # Check second curve in case it's better:
+
+        try:
+            j_from_v2 = interp1d(lx2, ly2)
+            v_from_j2 = interp1d(ly2, lx2)
+            voc2 = v_from_j2(0)
+            isc2 = j_from_v2(0)
+
+            temp_v2 = np.arange(min(lx2), max(lx2), (max(lx2)-min(lx2)/10000))
+            mpp_index2 = np.argmin(temp_v2*j_from_v2(temp_v2))
+            vm2 = temp_v2[mpp_index2]
+            im2 = j_from_v2(temp_v2[mpp_index2])
+
+            if im*vm > im2*vm2:
+                voc = voc2
+                isc = isc2
+                im = im2
+                vm = vm2
+        except:
+            pass
+            
+        ff = (vm*im)/(voc*isc)
+
+
+        voc_st_list.append(voc_st)
+        isc_st_list.append(isc_st)
+        ff_st_list.append(ff_st)
+        vm_st_list.append(vm_st)
+        im_st_list.append(im_st)
+        pce_st_list.append(abs(mpp_st))
+
+        voc_list.append(float(voc))
+        isc_list.append(float(isc))
+        ff_list.append(float(ff))
+        vm_list.append(float(vm))
+        im_list.append(float(im))
+        pce_list.append(abs(float(vm*im)))
         ids.append(db[key].get_id())
         ######################################################################
-
-        # Extract data from PixelData class for convenience:
-        dx1 = db[key].get_dark_iv()[0][0]
-        dy1 = [i *(1e3 / dark_area) for i in db[key].get_dark_iv()[0][1]]
-        dx2 = db[key].get_dark_iv()[1][0]
-        dy2 = [i *(1e3 / dark_area) for i in db[key].get_dark_iv()[1][1]]
-
-        lx1 = db[key].get_light_iv()[0][0]
-        ly1 = [i *(1e3 / area) for i in db[key].get_light_iv()[0][1]]
-        lx2 = db[key].get_light_iv()[1][0]
-        ly2 = [i *(1e3 / area) for i in db[key].get_light_iv()[1][1]]
-
         # Push lists into the cols on sheet, labels and type as appropriate:
         wks.from_list('A', lx1, 'Voltage', 'V', axis='X')
         wks.from_list('B', ly1, 'Current', 'mA/cm^2', "Illuminated, 1", axis='Y')
@@ -321,73 +367,22 @@ def origin_create_plots(db):
         wks.from_list('H', dy2, 'Current', 'mA/cm^2', "Dark, 2", axis='Y')
 
         # Create graph to plot into:
-        graph = op.new_graph(lname="IV: " + key, template='a4_template.otpu')
+        #graph = op.new_graph(lname="IV: " + key, template=op.path('u') + 'a4_template.otpu')
         # !!! a custom template is used in line above, change it to "line" if this is missing!!!
+        graph = op.new_graph(lname="IV: " + key, template='a4_template.otpu')
 
         # First 2 (solid line) plots on base layer:
         plot1 = graph[0].add_plot(wks, coly="F", colx="E", type='line')
         plot2 = graph[0].add_plot(wks, coly="B", colx="A", type='line')
         # Rescales axis, sets linewidth up, sets up autocolour:        
         op.lt_exec("Rescale; set %C -w 2000; layer -g; layer.X.showAxes=3; layer.Y.showAxes=3;")
-
-        # op.lt_exec("RECT1.DX={};RECT1.DY={};RECT1.X={};RECT1.Y={};RECT1.COLOR=4;RECT1.FILLCOLOR=7;RECT1.SHOW=1;".format(mpp_v/2,mpp_i/2,mpp_v/2,mpp_i/2))
+        
         layer2 = graph.add_layer(type="noxy")  # new layer, noax allows us to use the same axis as before
         plot3 = graph[1].add_plot(wks, coly="H", colx="G", type='line')
         plot4 = graph[1].add_plot(wks, coly="D", colx="C", type='line')
         # Rescales axis, sets linewidth up, Changes linestyle to dash, sets up autocolour:
         op.lt_exec("Rescale; set %C -w 2000; layer -g; set %C -d 1")
 
-        ######################################################################
-        # Fill factor rectangles: 
-
-        # Ideal:
-        voc_sweep = [voc * (i + 1) / 50 for i in range(50)]
-        isc_sweep = [isc * (1e3/area) * (i + 1) / 50 for i in range(50)]
-        v_ideal = []
-        i_ideal = []
-
-        for voc_step in voc_sweep:
-            v_ideal.append(voc_step)
-            i_ideal.append(isc* (1e3/area))
-        for isc_step in isc_sweep:
-            v_ideal.append(voc)
-            i_ideal.append(isc_step)
-
-        # Max power: 
-        v_mppt_sweep = [vm * (i + 1) / 50 for i in range(50)]
-        i_mppt_sweep = [im * (1e3/area) * (i + 1) / 50 for i in range(50)]
-        v_mppt_rect = []
-        i_mppt_rect = []
-
-        for voc_step in v_mppt_sweep:
-            v_mppt_rect.append(voc_step)
-            i_mppt_rect.append(im* (1e3/area))
-        for isc_step in i_mppt_sweep:
-            v_mppt_rect.append(vm)
-            i_mppt_rect.append(isc_step)
-
-        # Push to hidden worksheet:           
-        wks2 = op.new_sheet(lname="hacky_way_of_making_boxes(max_power)", hidden=True)
-        wks2.from_list('A', v_mppt_rect, axis='X')
-        wks2.from_list('B', i_mppt_rect, "Max power rectangle", axis='Y')
-        # Add to plot:
-        layer3 = graph.add_layer(type="noxy")  # new layer, noax allows us to use the same axis as before
-        plot5 = graph[2].add_plot(wks2, colx="A", coly="B", type='line')
-        op.lt_exec('Rescale; set %C -w 2000; set %C -d 5; set %c -cl color("Blue");')
-
-        wks3 = op.new_sheet(lname="hacky_way_of_making_boxes(ideal)", hidden=True)
-        wks3.from_list('A', v_ideal, axis='X')
-        wks3.from_list('B', i_ideal, "ideal power rectangle", axis='Y')
-
-        layer4 = graph.add_layer(type="noxy")  # new layer, noax allows us to use the same axis as before
-        plot6 = graph[3].add_plot(wks3, colx="A", coly="B", type='line')
-        op.lt_exec('Rescale; set %C -w 2000; set %C -d 1; set %c -cl color("#008800");')
-
-        # Add labels for Voc, Isc, ff
-        # Format: label -a "x" "y" "label";
-        op.lt_exec('label -a "' + str(voc + voc / 100) + '" "' + str(0) + '" "' + str(round(voc * 1000)) + ' mV";')
-        op.lt_exec('label -a "' + str(0) + '" "' + str(isc * 1e3/area) + '" "' + str(round(isc  * 1e3/area)) + ' mAcm^-2";')
-        op.lt_exec('label -a "' + str(vm / 2) + '" "' + str(im * (1e3/area) / 2) + '" "Fill Factor: \n ' + str(round(ff, 3)) + '";')
 
         ######################################################################
 
@@ -401,21 +396,44 @@ def origin_create_plots(db):
     op.lt_exec('pe_cd /; pe_cd "SUMMARY";')  # moves to summary dir
     wks_sum = op.new_sheet(lname="DATA SUMMARY ")  # Creates sheet for summary
     # super hacky way to get more cols but like leave me alone
-    for i in range(10):
+    for i in range(30):
         op.lt_exec('wks.addCol()')  # more hackyness, just forgive me
-
     # Info cols:
     wks_sum.from_list('A', [i['sys_label'] for i in ids], 'Cell', axis='X')
     wks_sum.from_list('B', [i['user_label'] for i in ids], 'Name', axis='X')
     wks_sum.from_list('C', [i['mux_index'] for i in ids], 'Device number', axis='X')
     # Data cols:
-    wks_sum.from_list('D', ff_list, 'Fill Factor', 'W', axis='Y')
-    wks_sum.from_list('E', vm_list, 'V_mp', 'V', axis='Y')
-    wks_sum.from_list('F', im_list, 'I_mp', 'A', axis='Y')
-    wks_sum.from_list('G', voc_list, 'V_oc', 'V', axis='Y')
-    wks_sum.from_list('H', isc_list, 'I_sc', 'A', axis='Y')
+    wks_sum.from_list('D', voc_list, 'V_oc', 'V', axis='Y')
+    wks_sum.from_list('E', isc_list, 'I_sc', 'A', axis='Y')
+    wks_sum.from_list('F', pce_list, 'Max power point', 'mWcm^-2', axis='Y')
+    wks_sum.from_list('G', ff_list, 'Fill Factor', '', axis='Y')
+    wks_sum.from_list('H', vm_list, 'V_mp', 'V', axis='Y')
+    wks_sum.from_list('I', im_list, 'I_mp', 'A', axis='Y')
+
+    # Get ready....here comes...MORE HACKYNESS
+    letters = ['J','K','L','M','N','O','P']
+    letter_counter = 0
+    
+    if voc_st_list[0]:
+        wks_sum.from_list(letters[letter_counter], voc_st_list, 'V_oc (STABALISED)', 'V', axis='Y')
+        letter_counter += 1
+    if isc_st_list[0]:
+        wks_sum.from_list(letters[letter_counter], isc_st_list, 'I_sc (STABALISED)', 'A', axis='Y')
+        letter_counter += 1
+    if pce_list[0]:
+        wks_sum.from_list(letters[letter_counter], pce_st_list, 'Max power point (STABALISED)', 'mWcm^-2', axis='Y')
+        letter_counter += 1
+    if ff_st_list[0]:
+        wks_sum.from_list(letters[letter_counter], ff_st_list, 'Fill Factor (STABALISED)', 'W', axis='Y')
+        letter_counter += 1
+    if vm_st_list[0]:
+        wks_sum.from_list(letters[letter_counter], vm_st_list, 'V_mp (STABALISED)', 'V', axis='Y')
+        letter_counter += 1
+    if im_st_list[0]:
+        wks_sum.from_list(letters[letter_counter], im_st_list, 'I_mp (STABALISED)', 'A', axis='Y')
+        letter_counter += 1
     # Hyperlinks to graphs:
-    wks_sum.from_list('I', graph_strs, 'IV curve', comments='CLICK the cell', axis='Z')
+    wks_sum.from_list(letters[letter_counter], graph_strs, 'IV curve', comments='CLICK the cell', axis='Z')
 
     return True
 
